e8a16b01c8d599a754d75d5aca1d794708ace168
Mils <emilsromanis@gmail.com>
2019-05-18 17:19:20 +0300

    update1

--- scripts/game.js
+++ scripts/game.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
         pause: false,
+        reverse: false,
         page_width: 0,
         page_height: 0,
         myParam: null,
@@ -28,18 +29,33 @@ var app = new Vue({
     mounted: function () {
         this.$nextTick(function () {
             window.addEventListener('resize', this.resize, true);
+            window.addEventListener("keyup", function(event) {
+                if (event.keyCode === 13) {
+                    this.explosion();
+                }
+            }.bind(this));
             this.startGame()
         })
     },
     methods: {
-        start: function() {
-            this.canvas = document.createElement("canvas");
-            this.canvas.width = this.page_width;                         
-            this.canvas.height = this.page_height;
-            this.context = this.canvas.getContext("2d");
-            document.getElementById("app").appendChild(this.canvas); 
-            this.frameNo = 0;
-            this.interval = setInterval(this.updateGameArea, 20);
+        addRect:function(){
+            this.tower.push(new Rectangle("red",600,120,this.page_width/2-300,0,4));
+        },
+        clear: function() {
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        },
+        explosion: function(){
+            if(this.tower.length==1) return;
+            if(this.tower.length>2){
+                this.pause = true;
+                this.reverse = true;
+            }
+            for(let i=2,len=this.tower.length;i<len;i++){
+                this.tower[i-1] = this.tower[i];
+                this.tower[i-1].free = true;
+                this.tower[i-1].period=0;
+            }
+            this.tower.pop();
         },
         resize: function() {
             this.page_width=$(window).width();
@@ -47,8 +63,15 @@ var app = new Vue({
             this.canvas.width = this.page_width;                         
             this.canvas.height = this.page_height;
         },
-        clear: function() {
-            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        start: function() {
+            this.canvas = document.createElement("canvas");
+            this.canvas.width = this.page_width;                         
+            this.canvas.height = this.page_height;
+            this.context = this.canvas.getContext("2d");
+            document.getElementById("app").appendChild(this.canvas); 
+            this.tower.push(new Rectangle("green",this.page_width,80,0,0,4));
+            this.frameNo = 0;
+            this.interval = setInterval(this.updateGameArea, 10);
         },
         startGame: function() {
             this.page_width=$(window).width();
@@ -57,12 +80,37 @@ var app = new Vue({
         },
         updateGameArea: function() {
             if(this.pause==false){
-                   
+                this.clear();
+                for(let i=0,len=this.tower.length;i<len;i++){
+                    if(this.tower[i].free==true){
+                        this.tower[i].period+=10;
+                        if(i==0){
+                            this.tower[i].collisionGround(this.page_height);
+                        }else this.tower[i].collision(this.tower[i-1]);
+                    }
+                    this.context.fillStyle = this.tower[i].color;
+                    this.context.fillRect(this.tower[i].x, this.tower[i].y, this.tower[i].width, this.tower[i].height);
+                }
+            }
+            if(this.reverse==true){
+                if(this.tower[1].speed-9*parseFloat(this.tower[1].period/1000)<=0){
+                    this.pause = false;
+                    this.reverse = false;
+                    for(let i=1,len=this.tower.length;i<len;i++){
+                        this.tower[i].period=0;
+                    }
+                }else{
+                    this.clear();
+                    this.context.fillStyle = this.tower[0].color;
+                    this.context.fillRect(this.tower[0].x, this.tower[0].y, this.tower[0].width, this.tower[0].height);
+                    for(let i=1,len=this.tower.length;i<len;i++){
+                        this.tower[i].period+=10;
+                        this.tower[i].y -= this.tower[i].speed-9*parseFloat(this.tower[i].period/1000);
+                        this.context.fillStyle = this.tower[i].color;
+                        this.context.fillRect(this.tower[i].x, this.tower[i].y, this.tower[i].width, this.tower[i].height);
+                    }
+                }
             }
         },
-        drawcBlock: function(){
-            context.fillStyle = "#FF0000";
-            context.fillRect(0, 0, 150, 75);
-        }
     }
 })
