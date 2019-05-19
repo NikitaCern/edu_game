168fdc6e50359033d3aa68b9f135b8d319a7cd01
Nikita <nikita.cernisovs@gmail.com>
2019-05-19 11:58:13 +0300

    game update

--- scripts/game.js
+++ scripts/game.js
@@ -18,11 +18,7 @@ var app = new Vue({
         },
         height_input:1,
         difficulty_input:1,
-<<<<<<< HEAD
-=======
-        nr_of_chars:1,
-        lable: [null,null]
->>>>>>> aa66eebde25941cb1c59bf4fa6f1c64ceb61d821
+        lable:[null,null]
     },
     created: function () {
         var url = new URL(window.location);
@@ -237,13 +233,8 @@ var app = new Vue({
                 var answer = n*m;
               }
               if(chosen_operator == "/"){
-<<<<<<< HEAD
                 var m = this.getRandomLimits(Math.pow(10 , nr_of_chars));
                 var n = m*this.getRandomLimits(Math.pow(10 , nr_of_chars));
-=======
-                var m = this.getRandomLimits(Math.pow(10 , this.nr_of_chars));
-                var n = m*this.getRandomLimits(Math.pow(10 , this.nr_of_chars));
->>>>>>> aa66eebde25941cb1c59bf4fa6f1c64ceb61d821
                 var  answer=n/m;
                 var quest = n+"/"+m+"=";
               }
